693c3c9cda2c54f8793236e51ddfb5cdb6842b6a
Jesse Brink <jbrink0925@gmail.com>
2021-06-12 13:23:39 -0400

    add comments to js file

--- part_two.js
+++ part_two.js
@@ -2,8 +2,10 @@ const $ul = $('.ratings ul');
 const $submitButton = $('#submit');
 const $title = $('#title');
 const $rating = $('#rating');
+// create a counter for giving each li and remove button a unique id.
 let i = 0;
 
+// prevent form submission and append a new li and button to the ul.
 $submitButton.on('click', (evt) => {
     evt.preventDefault();
     const $newLi = $(`<li id="li${i}">${$title.val()} | Rating: ${$rating.val()}<button type="submit" id=remove${i}>Remove</button></li>`);
@@ -11,6 +13,7 @@ $submitButton.on('click', (evt) => {
     i++;
 });
 
+// event listener on the ul to handle removing the li and button from the ul on click.
 $ul.on('click', (evt) => {
     if (evt.target.id.includes('remove')) {
         const idNumber = evt.target.id.slice(6);
